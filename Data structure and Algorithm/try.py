f1d54268afb7ee80e353be72b0427da27a92ed9b
Judy Gao <gaoyuchen28@gmail.com>
2026-02-06 19:08:09 +0800

    str

--- Data structure and Algorithm/try.py
+++ Data structure and Algorithm/try.py
@@ -1,9 +1,12 @@
-def my_generator(m):
-    if m==5:
-        yield 1
-        yield 2
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-g = my_generator(5)
-print(type(g))  
-print(my_generator(5)) 
-print(my_generator(5))
+    def __str__(self):
+        return f"{self.name} is {self.age} years old."
+dog1 = Dog("Buddy", 3)
+dog2 = Dog("Charlie", 5)
+
+print(dog1)  
+print(dog2)
