5854ee09659b8fa1eac17904d8b45e3cfdfc8369
Judy Gao <gaoyuchen28@gmail.com>
2026-02-06 19:08:54 +0800

    instance variable and class variable

--- Data structure and Algorithm/try.py
+++ Data structure and Algorithm/try.py
@@ -1,12 +1,26 @@
 class Dog:
+    # Class variable
+    species = "Canine"
+
     def __init__(self, name, age):
+        # Instance variables
         self.name = name
         self.age = age
 
-    def __str__(self):
-        return f"{self.name} is {self.age} years old."
+# Create objects
 dog1 = Dog("Buddy", 3)
 dog2 = Dog("Charlie", 5)
 
-print(dog1)  
-print(dog2)
+# Access class and instance variables
+print(dog1.species)  # (Class variable)
+print(dog1.name)     # (Instance variable)
+print(dog2.name)     # (Instance variable)
+
+# Modify instance variables
+dog1.name = "Max"
+print(dog1.name)     # (Updated instance variable)
+
+# Modify class variable
+Dog.species = "Feline"
+print(dog1.species)  # (Updated class variable)
+print(dog2.species)
