df47b694890992bcdc8bc7622ce8a6e5f43e4a84
Judy Gao <gaoyuchen28@gmail.com>
2026-02-12 16:52:19 +0800

    question_3

--- Data structure and Algorithm/try.py
+++ Data structure and Algorithm/try.py
@@ -1,10 +1,13 @@
-# 编写一个程序计算商店的利润/亏损额。
+# 编写一个程序来计算一个人的BMI。
 
-# 输入两个整数，分别为销售价格cost_price和成本价格selling_price。
-# 计算利润或亏损额（即“销售价格-成本价格”）。
-# 注意：负数意味着损失。
+# BMI计算公式：
 
-cost_price = int(input())
-selling_price = int(input())
-print(selling_price-cost_price)
+# BMI = 体重/身高^2
+# 输入一个浮点数表示人的体重（单位：公斤），并把它赋给体重指标weight。
+# 输入一个浮点数表示人的身高（以米为单位），赋予其分配给身高指标height。
+# 使用公式来计算BMI，并输出。
+
+weight = float(input())
+height = float(input())
+print(weight/(height*height))
 
