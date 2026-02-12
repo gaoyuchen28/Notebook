7bffa9a2af40367573ddc1b1ce3ce03788cefe44
Judy Gao <gaoyuchen28@gmail.com>
2026-02-12 17:00:54 +0800

    f-string避免多余的空格出现

--- Data structure and Algorithm/try.py
+++ Data structure and Algorithm/try.py
@@ -1,13 +1,10 @@
-# 编写一个程序来计算一个人的BMI。
+# 编写一个程序，按如下格式打印日期：
 
-# BMI计算公式：
+# 输入三个字符串，分别表示“日”、“月”、和“年”。
+# 以格式月/日/年打印日期。
 
-# BMI = 体重/身高^2
-# 输入一个浮点数表示人的体重（单位：公斤），并把它赋给体重指标weight。
-# 输入一个浮点数表示人的身高（以米为单位），赋予其分配给身高指标height。
-# 使用公式来计算BMI，并输出。
-
-weight = float(input())
-height = float(input())
-print(weight/(height*height))
+day = input()
+month = input()
+year = input()
+print(f"{month}/{day}/{year}") 
 
