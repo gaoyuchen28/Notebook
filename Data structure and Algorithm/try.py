2f9f09cc2e0a1c172cfbece5aa9d45f9a8afceb4
Judy Gao <gaoyuchen28@gmail.com>
2026-02-12 16:46:13 +0800

    question_1

--- Data structure and Algorithm/try.py
+++ Data structure and Algorithm/try.py
@@ -1,7 +1,14 @@
-d = {1: 'Geeks', 'name': 'For', 3: 'Geeks'}
+# 假设你是一位出色的 AA 餐厅服务员，你的任务是计算每位顾客的应付金额。
 
-# Accessing an element using key
-print(d['name'])
+# 输入顾客人数，并赋予total_friends变量。
+# 输入总发票数值，并分配给total_bill数值。
+# 在付款费用上加上20%的税，并计算最终付款费用均摊给付款额，然后打印。
+# 输入格式
 
-# Accessing a element using get
-print(d.get('names'))
+# 两个整数
+
+total_friends=int(input())
+total_bill = int(input())
+total_bill = total_bill*(1+0.2)
+print(total_bill/total_friends)
+
