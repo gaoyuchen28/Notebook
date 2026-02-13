c73321841fe649e42b8718d9e87599ee4238e9a2
Judy Gao <gaoyuchen28@gmail.com>
2026-02-13 12:04:54 +0800

    two type of argument

--- Data structure and Algorithm/try.py
+++ Data structure and Algorithm/try.py
@@ -1,13 +1,9 @@
-# 编写一个程序：将N个巧克力均分给M个儿童。
+def nameAge(name, age):
+    print("Hi, I am", name)
+    print("My age is ", age)
 
-# 从输入获取一个整数：巧克力数量，并将其分配给 chocolates 变量。
-# 从输入获取一个整数：孩子数量，并将其分配给 children 变量。
-# 计算每个孩子在除法后得到的巧克力数量并打印出来。
-# 计算剩余巧克力的数量并打印出来。
+print("Case-1:")
+nameAge("Suraj", 27)
 
-chocolates = int(input())
-
-children = int(input())
-
-print(chocolates // children)
-print(chocolates % children)
+print("\nCase-2:")
+nameAge(name="Suraj", age="27")
