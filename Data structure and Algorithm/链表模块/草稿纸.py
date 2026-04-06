1f3df6db099b00b9d34a0770a3eff1176efa16e7
Judy Gao <gaoyuchen28@gmail.com>
2026-04-06 17:31:27 +0800

    链表合并_1

--- Data structure and Algorithm/链表模块/草稿纸.py
+++ Data structure and Algorithm/链表模块/草稿纸.py
@@ -1,11 +1,17 @@
-# 链表反转（Reverse Linked List）
+# 链表合并
 
-def reverse(head):
-    current = head
-    prev = None
-    while current:
-        node = current.next
-        current.next = prev
-        prev = current
-        current = node
-    return prev
+def merge_sorted_lists(l1, l2):
+    dummy = Node(0)
+    tail = dummy
+    while l1 and l2:
+        if l1.data > l2.data:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+    if l1:
+        tail.next = l1
+    else:
+        tail.next = l2
+    return dummy.next
