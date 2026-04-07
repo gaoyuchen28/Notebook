3962b2b58e482ff31105d056b8fbdfc3eda44503
Judy Gao <gaoyuchen28@gmail.com>
2026-04-07 19:57:34 +0800

    反转字符串

--- Data structure and Algorithm/链表模块/草稿纸.py
+++ Data structure and Algorithm/链表模块/草稿纸.py
@@ -1,17 +1,11 @@
-# 斐波那契数列
+# 反转字符串
 
-def Fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+def reverse(arr):
+    if len(arr) == 1:
+        return arr[0]
     else:
-        return Fib(n-1) + Fib(n-2)
+        return reverse(arr[1:]) + arr[0]
 
-n = int(input())
-data = []
-for i in range(n):
-    data.append(Fib(i)) # 用append！！！
+arr = list(input().split(", "))
 
-for num in data:
-    print(num, end = " ")
+print(reverse(arr))
