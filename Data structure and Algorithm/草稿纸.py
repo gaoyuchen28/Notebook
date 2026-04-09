c4b970bdbc2eff4a1afd19898547c37803785c46
Judy Gao <gaoyuchen28@gmail.com>
2026-04-09 12:30:37 +0800

    计算比特数量

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,15 +1,11 @@
-# Check for Palindrome
-def check(left, right):
-    if  left >= right:
-        print("True")
-        return True
-    elif n[left] != n[right]:
-        print("False")
-        return False
+# Count Set-bits of number
+def count(n):
+    if n == 0:
+        return 0
+    if (n & 1) == 1:
+        return 1 + count(n>>1)
     else:
-        check(left+1,right-1)
+        return count(n>>1)
 
-n = str(input())
-left = 0
-right = len(n)-1
-check(left, right)
+n = int(input())
+print(count(n))
