e96c0a9d404296b21681724c60f33f4b9c12d371
Judy Gao <gaoyuchen28@gmail.com>
2026-04-09 13:47:43 +0800

    Binary to Gray code

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,17 +1,13 @@
-# Sort the Queue
-def Findmin(n):
-    if len(n) == 1:
-        c.append(n[0])
-        return
+# Binary to Gray code
+def convert(s):
+    n = len(s)
+    if n == 1:
+        return str(s[0])
     else:
-        for i in range(len(n)):
-            if n[0] > n[i]:
-                n[0], n[i] = n[i], n[0]
-        Findmin(n[1:])
-        c.append(n[0])
-        return
-
-c = []
-n = list(map(int, input().split(", ")))
-Findmin(n)
-print(c)
+        result = s[n-1]^s[n-2]
+        return convert(s[:(n-1)]) + str(result)
+n = str(input())
+s = []
+for i in range(len(n)):
+    s.append(int(n[i]))
+print(convert(s))
