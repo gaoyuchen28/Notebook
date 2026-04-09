7cf6f112a44cec20d16b9b813d5d607982ed404f
Judy Gao <gaoyuchen28@gmail.com>
2026-04-09 10:31:20 +0800

    add two numbers

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,21 +1,23 @@
-# 例题: 爬楼梯
-# 树老师爬楼梯，他可以每次走1级或者2级，输入楼梯的级数，求不同的走法数
-# 例如：楼梯一共有3级，他可以每次都走一级，或者第一次走一级，第二次走两级，
-# 也可以第一次走两级，第二次走一级，一共3种方法。
+# 例题:  Add Two Numbers
 
 
-
-def count(n): # i 表示已经压入了多少个数，stack_size 表示当前栈内元素个数
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
+def addTwoNumbers(l1, l2):
+    if len(l1) == 1 or len(l2) == 1:
+        result = l1[0] + l2[0]
+        n.append(result)
+        return
     else:
-        return count(n-1) + count(n-2)
-    
+        if l1[0] + l2[0] >= 10:
+            result = (l1[0] + l2[0])%10
+        else:
+            result = l1[0] + l2[0]
+        addTwoNumbers(l1[1:],l2[1:])
+        n.append(result)
 
-n = int(input())
-print(count(n))
+n = []
+l1 = list(map(int, input().split(",")))
+l2 = list(map(int, input().split(",")))
+
+addTwoNumbers(l1, l2)
     
+print(n)
