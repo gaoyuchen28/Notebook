e14c577a221195d9ac103d4492607d6c9675ad12
Judy Gao <gaoyuchen28@gmail.com>
2026-04-12 21:51:22 +0800

    Tower of Hanoi

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,16 +1,10 @@
-# Length of longest palindromic sub-string
-def test(n, left, right, count):
-    if left > right:
-        return count
-    if left == right:
-        return count +1
-    if n[left] == n[right]:
-        count = test(n, left+1, right-1, count+2)
-        return max(count, max(test(n, left+1, right, 0), test(n,left, right-1,0)))
-    return max(test(n, left+1, right, 0), test(n,left, right-1,0))
+# Tower of Hanoi
+def Hanoi(n, fromTower, withTower, toTower):
+    if n == 0:
+        return
+    Hanoi(n-1, fromTower, toTower, withTower)
+    print("Disk ", n, "moved from ", fromTower,"to ",toTower)
+    Hanoi(n-1, withTower, fromTower, toTower)
 
-if __name__ == "__main__":
-    str = "abca"
-    n = len(str)
-    
-    print(test(str, 0, n - 1, 0))
+n=3
+Hanoi(n, 'A','B','C')
