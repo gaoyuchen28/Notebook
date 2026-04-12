3696a770ae9e834abbb1f7e4ce5cd5dfee755b91
Judy Gao <gaoyuchen28@gmail.com>
2026-04-12 23:13:40 +0800

    Find geometric sum of the series

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,15 +1,9 @@
-# Program for nCr
-def Calculate(n, r):
-  if n == r:
+# Find geometric sum of the series
+def Calculate(n):
+  if n == 0:
     return 1
-  elif n < r:
-    return 0
   else:
-    if r == 0:
-        return 1
-    if r == 1:
-        return n 
-    else:
-        return Calculate(n-1,r) + Calculate(n-1,r-1)
+    return 1 / pow(3, n) + Calculate(n-1)
 
-print(Calculate(6,3))
+print(Calculate(5))
+    
