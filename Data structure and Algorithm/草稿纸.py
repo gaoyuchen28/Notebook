23f6bb5da87b586d1b5107bf948915d04ea5e62d
Judy Gao <gaoyuchen28@gmail.com>
2026-04-12 21:44:01 +0800

    *Length of longest palindromic sub-string

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,11 +1,16 @@
-# Printing Pyramid Patterns
-def prints(num):
-    if num == 1:
-        print("*")
-        return
-    else:
-        prints(num-1)
-        for i in range(num):
-            print("*", end = " ")
-        print("\n", end = "")
-prints(5)
+# Length of longest palindromic sub-string
+def test(n, left, right, count):
+    if left > right:
+        return count
+    if left == right:
+        return count +1
+    if n[left] == n[right]:
+        count = test(n, left+1, right-1, count+2)
+        return max(count, max(test(n, left+1, right, 0), test(n,left, right-1,0)))
+    return max(test(n, left+1, right, 0), test(n,left, right-1,0))
+
+if __name__ == "__main__":
+    str = "abca"
+    n = len(str)
+    
+    print(test(str, 0, n - 1, 0))
