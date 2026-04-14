6ea1e7f7a7109c13659d2fdc356ced5098adca28
Judy Gao <gaoyuchen28@gmail.com>
2026-04-14 18:02:42 +0800

    F2: Length of longest palindromic sub-string

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,8 +1,18 @@
-# Convert a String to an Integer
-def Convert(str):
-    if len(str) == 1:
-        return int(str[0])
-    else:
-        return 10*Convert(str[:(len(str)-1)]) + int(str[len(str)-1])
+# Length of longest palindromic sub-string
+def Length(s):
+    n = len(s)
+    max_len = 0
+    def expand(left,right):
+        while left >= 0 and right < n and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
+    i = 0
+    while i < n:
+        len1 = expand(i,i)
+        len2 = expand(i,i+1)
+        max_len = max(max_len, len1, len2)
+        i+=1
+    return max_len
 
-print(Convert("02345"))
+print(Length("aaaabbaa"))
