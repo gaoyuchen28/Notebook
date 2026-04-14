6416a8fd956bd27606979e1d2d96e15e61c6a699
Judy Gao <gaoyuchen28@gmail.com>
2026-04-14 22:29:13 +0800

    电影节

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,29 +1,23 @@
-# 圣诞礼物
+# 电影节
 
-eps = 1e-6
-
-class Candy:
-    def __init__(self, v, w):
-        self.v = v
-        self.w = w
+class Movie:
+    def __init__(self, s, e):
+        self.s = s
+        self.e = e
     def __lt__(self, other):
-        return (self.v/self.w - other.v/other.w)>eps
+        return self.e < other.e
 
-n, w = list(map(int, input().split()))
-
-candies = [Candy(0,0) for i in range(n)]
+n = int(input())
+mov = [Movie(0,0) for i in range (n)]
 for i in range(n):
-    candies[i].v, candies[i].w = list(map(float, input().split()))
+    mov[i].s , mov[i].e = list(map(int, input().split()))
 
-candies.sort()
-totalv = 0
-totalw = 0
+mov.sort()
+result = 1
+e = mov[0].e
 for i in range(n):
-    if totalw + candies[i].w <= w:
-        totalv += candies[i].v
-        totalw += candies[i].w
-    else:
-        totalv += candies[i].v * (w - totalw)/candies[i].w # 搁不下一整箱了还可以换
-        break
+    if mov[i].s >= e:
+        result += 1
+        e = mov[i].e
 
-print('%.1f'%totalv)
+print(result)
