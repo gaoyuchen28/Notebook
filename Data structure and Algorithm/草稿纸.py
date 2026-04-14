c3426d703226f65116e679df800073e93c0f73df
Judy Gao <gaoyuchen28@gmail.com>
2026-04-14 22:54:22 +0800

    找零兑换优化

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,14 +1,18 @@
-# 找零兑换
+# 找零兑换优化
 
-def recMc(coinlist, change):
+def recMc(coinlist, change, knowresult):
     mincoin = change
     if change in coinlist:
+        knowresult[change] = 1
         return 1
+    elif knowresult[change] > 0:
+        return knowresult[change]
     else:
         for i in [c for c in coinlist if c <= change]:
-            numCoin = 1 + recMc(coinlist, change - i)
+            numCoin = 1 + recMc(coinlist, change - i, knowresult)
             if numCoin < mincoin:
                 mincoin = numCoin
+                knowresult[change] = mincoin
     return mincoin
 
-print(recMc([1,5,10,25],63))
+print(recMc([1,5,10,25],63, [0]*64))
