90ae7febc7b4ac979ba3fa0392fd06b747a241c3
Judy Gao <gaoyuchen28@gmail.com>
2026-04-14 19:26:39 +0800

    *分发糖果

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,28 +1,21 @@
-# N皇后问题
+# 分发糖果
 
-def isok(n, pos):
-    for i in range(n):
-        if result[i] == pos or abs(i - n) == abs(result[i] - pos):
-            return False
-    
-    return True
+def candy(ratings):
+    cds = [1]*len(ratings)
+    for i in range(1,len(ratings)):
+        if ratings[i-1] < ratings[i]:
+            cds[i] = cds[i-1]+1
+        elif ratings[i-1] == ratings[i]:
+            cds[i] = 1
+        else: # 如果比前面低的话要讨论很多方案
+            cds[i] = 1
+            if cds[i-1] == 1:
+                for k in range(i-1, -1, -1): # 这个必须要倒叙
+                    cds[k] += 1
+                    if k > 0 and (ratings[k] >= ratings[k-1] or cds[k]>cds[k-1]):
+                        break
+    return cds
 
-def queen(N, m):
-    if N == m:
-        for i in range(N):
-            print(result[i], end = " ")
-        print(" ")
-        return True
-    else:
-        succeed = False
-        for i in range(N):
-            if isok(m,i):
-                result[m]=i
-                succeed = queen(N, m+1) or succeed
-        return succeed
+ratings = "122"
 
-
-n = int(input())
-result = [-1]*n
-queen(n, 0)
-print()
+print(candy(ratings))
