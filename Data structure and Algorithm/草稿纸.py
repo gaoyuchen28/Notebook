d2ac59a162ab514912b45bac121d9bdb53db9475
Judy Gao <gaoyuchen28@gmail.com>
2026-04-21 08:27:30 +0800

    利用动态规划解决找零问题_2(返回找钱组合)

--- Data structure and Algorithm/草稿纸.py
+++ Data structure and Algorithm/草稿纸.py
@@ -1,13 +1,25 @@
-# 利用动态规划解决找零问题_1
+# 利用动态规划解决找零问题_2(返回找钱组合)
 
 
-def dpMakeChange(change, coinList, mincoin):
+def dpMakeChange(change, coinList, mincoin, coinUsed):
     for cent in range(1,change+1):
         count = cent
+        newcoin = 1
         for i in [c for c in coinList if c <= cent]:
             if mincoin[cent - i] + 1 < count:
                 count = mincoin[cent - i] + 1
+                newcoin = i
         mincoin[cent] = count
+        coinUsed[cent] = newcoin
     return mincoin[change]
 
-print(dpMakeChange(63, [1,5,10,21,25], [0]*64))
+def printChange(coinUsed, change):
+    coin = change
+    while coin > 0:
+        print(coinUsed[coin],end = ", ")
+        coin = coin - coinUsed[coin]
+
+coinUsed = [0]*64
+
+dpMakeChange(63, [1,5,10,21,25], [0]*64, coinUsed)
+printChange(coinUsed, 63)
